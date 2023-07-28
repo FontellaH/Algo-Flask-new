5fe87fc45b2b4432ceaa82fa57968395a11eb162
Fontella Hooper <hooperfontella6@gmail.com>
2023-07-28 09:21:14 -0700

    new algo

---
+++ Wk2_Day_1.js
@@ -0,0 +1,51 @@
+/* 
+  Given a string,
+  return a new string with the duplicate characters excluded
+  Bonus: Keep only the last instance of each character.
+*/
+
+const str1 = "abcABCabcABCabcABC";
+const expected1 = "abcABC";
+
+const str2 = "helloo";
+const expected2 = "helo";
+
+const str3 = "";
+const expected3 = "";
+
+const str4 = "aa";
+const expected4 = "a";
+
+//bonus
+const str5 = "aba"
+const expected5 = "ba" // ab
+
+/**
+ * De-dupes the given string.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} str A string that may contain duplicates.
+ * @returns {string} The given string with any duplicate characters removed.
+ * An object may prove useful here
+ */
+function stringDedupe(str) {
+    //Your code here
+}
+let expectes =""
+for (var i = 0; i < str.length; i++) {
+    var newstr = str[i];
+    if (expected1.includes(newstr)){
+
+
+    }
+    else{
+        expected = expected.concat(newstr)
+    }
+    return expected;
+}
+
+console.log(stringDedupe(str1));
+console.log(stringDedupe(str2));
+console.log(stringDedupe(str3));
+console.log(stringDedupe(str4));
+console.log(stringDedupe(str5));
